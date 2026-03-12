9d91b0e99585f23fa9a8db94de6f63b8eb2b8a28
Marlene Rietz <106233712+marleriee@users.noreply.github.com>
2026-03-12 15:07:56 +0100

    Update bootstrap_cox_function.R

--- R/bootstrap_cox_function.R
+++ R/bootstrap_cox_function.R
@@ -1,74 +1,65 @@
 #' The bootstrap_cox function
 #'
 
-bootstrap_cox_function <- function(variables, number, model_data, ndraws, l = "lambda.1se") {
-  #create empty dataframe with names of all variables with >60,000 complete cases
+bootstrap_cox_function <- function(variables, number, model_data, ndraws,
+                                   l = "lambda.1se") {
   set.seed(4321)
 
   vals <- dfVulcanoTwo %>%
-    mutate(term = gsub("0.*", "0", term)) %>%
+    mutate(term = sub("0[A-Z]*$", "0", term)) %>%
     filter(term %in% variables) %>%
     filter(casesComplete > number) %>%
-    select(term)
+    dplyr::select(term)
 
-  extract_names <- coxph(as.formula(paste("Surv(time, dmNewDig) ~", paste0(gsub("0.*","", vals[,1]), "0", collapse = " + "))), dataCon) %>%
+  extract_names <- coxph(
+    as.formula(paste("Surv(time, event_selected) ~",
+                     paste(sub("0[A-Za-z]*$", "0", vals[, 1]), collapse = " + "))),
+    dataCon) %>%
     tidy()
 
-  extract_names_2 <- extract_names[,1]
-
-  coef_mtx <- extract_names_2
+  coef_mtx <- extract_names[, 1]
 
   for (i in 1:ndraws) {
+    bootstrap_ids   <- sample(seq(nrow(model_data)), nrow(model_data), replace = TRUE)
+    bootstrap_data  <- model_data[bootstrap_ids, ]
 
-    #Drawing out a random subset of id's
-    #Because of replace = TRUE --> one case can be selected mulitple times
-    bootstrap_ids <- sample(seq(nrow(model_data)),
-                            nrow(model_data),
-                            replace = TRUE)
-
-    #Selecting the generated id's from the full dataset
-    bootstrap_data <- model_data[bootstrap_ids,]
-
-    #select all complete cases with variables >60,000 cases in full dataset
     dfGroup <- bootstrap_data %>%
-      dplyr::select("dmNewDig", "time", "age0", "sex0", vals$term) %>%
+      dplyr::select("event_selected", "time", "age0", "sex0", vals$term) %>%
       na.omit()
 
-    #Selecting lambda (lambda.1se for leniency)
-    m1x <- model.matrix( ~ . -1 - time - dmNewDig, dfGroup)
-    m1y <- Surv(dfGroup$time, 1-(dfGroup$dmNewDig-1))
-    m1bestL <- cv.glmnet(m1x, m1y,  family = "cox", alpha = 1, nfolds=10)
-    m1e <- glmnet(m1x, m1y,  family="cox", lambda = m1bestL[[l]], alpha = 1)
+    m1x     <- model.matrix(~ . -1 - time - event_selected, dfGroup)
+    m1y     <- Surv(dfGroup$time, dfGroup$event_selected)
+    m1bestL <- cv.glmnet(m1x, m1y, family = "cox", alpha = 1, nfolds = 10)
+    m1e     <- glmnet(m1x, m1y, family = "cox", lambda = m1bestL[[l]], alpha = 1)
 
-    #Running model with "best" lambda
-    bootstrap_model <- coxph(as.formula(paste("Surv(time, dmNewDig) ~", paste0(gsub("0.*","", names(as.matrix(coef(m1e))[as.matrix(coef(m1e))[,1] != 0,])), "0", collapse = " + "))), bootstrap_data)
+    coef_b       <- as.matrix(coef(m1e))
+    sel_b        <- rownames(coef_b)[!is.na(coef_b[, 1]) & coef_b[, 1] != 0]
+    if (length(sel_b) == 0) next
+
+    bootstrap_model <- coxph(
+      as.formula(paste("Surv(time, event_selected) ~",
+                       paste(sub("0[A-Za-z]*$", "0", sel_b), collapse = " + "))),
+      bootstrap_data)
 
     model_tidy <- tidy(bootstrap_model, exponentiate = T) %>%
       dplyr::select(term, p.value) %>%
       rename(i = p.value)
 
-    #Storing p-values in dataframe
     coef_mtx <- coef_mtx %>% merge(., model_tidy, by = "term", all = T)
-
   }
 
   trans <- function(x){
     x %>%
       mutate_at(.vars = vars(contains("model")),
                 .funs = funs(as.numeric(case_when(
-                  . < 0.05 ~ "1",
+                  .  < 0.05 ~ "1",
                   . >= 0.05 ~ "0",
-                  TRUE ~ NA_character_
-                )))) %>%
-      mutate(total = select(., contains("model")) %>%  rowSums(na.rm = TRUE)) %>%
+                  TRUE      ~ NA_character_)))) %>%
+      mutate(total = dplyr::select(., contains("model")) %>% rowSums(na.rm = TRUE)) %>%
       dplyr::select(term, total) %>%
       arrange(desc(total))
   }
 
-
   colnames(coef_mtx) <- c("term", paste("model", seq(1, ndraws, by = 1), sep = ""))
-
-  out <- trans(coef_mtx)
-
-  return(out)
+  trans(coef_mtx)
 }
